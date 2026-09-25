6230266a5030dd48cd9ad0d1a15b3f26c2bac838
mr-thanhan-dev <mrthanh1999.dev@gmail.com>
2026-09-25 07:01:08 +0700

    Init project

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cp "$DIR/zc339a-watchdog.service" /etc/systemd/system/zc339a-watchdog.service
 systemctl daemon-reload
 systemctl enable --now zc339a-watchdog.service
 
-echo "=== [3/4] Installing Uptime Logger Service ==="
+echo "=== [3/5] Installing Uptime Logger Service ==="
 cp "$DIR/zc339a-uptime-logger.sh" /usr/local/sbin/zc339a-uptime-logger.sh
 chmod +x /usr/local/sbin/zc339a-uptime-logger.sh
 
@@ -28,7 +28,10 @@ cp "$DIR/zc339a-uptime.service" /etc/systemd/system/zc339a-uptime.service
 systemctl daemon-reload
 systemctl enable --now zc339a-uptime.service
 
-echo "=== [4/4] Verifying Services ==="
+echo "=== [4/5] Configuring Auto-Login and Disabling DPMS/Screen Blanking ==="
+bash "$DIR/disable_dpms_autologin.sh"
+
+echo "=== [5/5] Verifying Services ==="
 echo "Watchdog service: $(systemctl is-active zc339a-watchdog.service)"
 echo "Uptime service:   $(systemctl is-active zc339a-uptime.service)"
 
